8b3266b6ce1bc6e5b9c9cfdfa1e264ec434d44c0
lukasgeis <lukasgeis@gmx.de>
2024-12-28 14:56:31 +0100

    sort scaled gifts by color

--- scripts/gift.gd
+++ scripts/gift.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
-@export var size: Vector2i
+@export var gift_size: Vector2i
+@export var scale_size: int = 1
 @export var texture: Texture2D
 @export var color: int
+
+@onready var size: Vector2i = gift_size * scale_size
 
 var sprite: Sprite2D
 
@@ -23,13 +26,14 @@ func _ready() -> void:
 	
 	add_child(sprite)
 	
+	scale = Vector2(scale_size, scale_size)
 	for c in $Area2D.get_children():
 		if c is CollisionShape2D:
 			var shape = RectangleShape2D.new()
-			shape.size = (size * Globals.TILE_SIZE) as Vector2
+			shape.size = (gift_size * Globals.TILE_SIZE) as Vector2
 			c.shape = shape
 	
-	$Area2D.position += (size * Globals.TILE_SIZE) as Vector2 / 2
+	$Area2D.position += (gift_size * Globals.TILE_SIZE) as Vector2 / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -42,8 +46,12 @@ func _drag():
 		offset = get_global_mouse_position() - global_position
 		Globals.is_dragging = true
 		Globals.current_gift = self
+		
+		_on_select_gift()
 	if Input.is_action_pressed("click"):
 		global_position = get_global_mouse_position() - offset
+		
+		_on_drag_gift()
 	elif Input.is_action_just_released("click"):
 		Globals.is_dragging = false
 		if Globals.current_grid:
@@ -53,30 +61,50 @@ func _drag():
 			tween.tween_property(self, "position", Globals.current_grid.gift_global_position as Vector2, 0.2).set_ease(Tween.EASE_OUT)
 			Globals.current_grid.place_rect(Rect2i(Globals.current_grid.gift_position, size))
 			
+			_on_place_gift()
 			$Area2D.process_mode = Node.PROCESS_MODE_DISABLED
 		else:
 			tween.tween_property(self, "global_position", initial_position, 0.2).set_ease(Tween.EASE_OUT)
+			_on_reject_gift()
 		Globals.current_gift = null
+		Globals.current_grid = null
 
+func _on_select_gift():
+	pass
+	
+func _on_drag_gift():
+	pass
+
+func _on_place_gift():
+	pass
+	
+func _on_reject_gift():
+	pass
 
 func _on_area_2d_mouse_entered() -> void:
 	if not Globals.is_dragging:
 		draggable = true
-		scale = Vector2(1.05, 1.05)
+		scale = Vector2(scale_size + 0.05, scale_size + 0.05)
+		Globals.current_gift = self
 
 
 func _on_area_2d_mouse_exited() -> void:
 	if not Globals.is_dragging:
 		draggable = false
-		scale = Vector2(1, 1)
+		scale = Vector2(scale_size, scale_size)
+		Globals.current_gift = null
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if color != body.color:
+		return
 	Globals.current_grid = body
 	Globals.current_grid.gift_inside = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
+	if color != body.color:
+		return
 	can_be_dropped = false
 	Globals.current_grid.gift_inside = false
 	Globals.current_grid = null
--- scripts/packing_grid.gd
+++ scripts/packing_grid.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 @export var empty_state: Texture2D
 @export var full_state: Texture2D
+@export var color: int
 
 var grid_states: Array[bool] = []
 var grid_size: Vector2i = Vector2i(8, 8)
+
+var placed_tiles: int = 0
 
 var textures: Array[Sprite2D] = []
 
@@ -18,7 +21,7 @@ func _ready() -> void:
 	shape.size = (grid_size * Globals.TILE_SIZE) as Vector2
 	$CollisionShape2D.shape = shape
 	$CollisionShape2D.position += (grid_size * Globals.TILE_SIZE) as Vector2 / 2
-	_reset_grid()
+	reset_grid()
 	
 	for x in range(grid_size.x):
 		for y in range(grid_size.y):
@@ -28,18 +31,17 @@ func _ready() -> void:
 			sprite.offset = Vector2(x, y) * (Globals.TILE_SIZE as Vector2)
 			add_child(sprite)
 			textures.append(sprite)
-	
-	place_rect(Rect2i(1, 2, 3, 4))
 
-func _reset_grid():
+func reset_grid():
 	grid_states.clear()
 	for i in range(grid_size.x * grid_size.y):
 		grid_states.append(false)
+	placed_tiles = 0
 
 func is_rect_placeable(rect: Rect2i) -> bool:
 	for x in range(rect.position.x, rect.end.x):
 		for y in range(rect.position.y, rect.end.y):
-			if grid_states[y + x * grid_size.x]:
+			if x < 0 or y < 0 or x >= grid_size.x or y >= grid_size.y or grid_states[y + x * grid_size.x]:
 				return false
 	
 	return true
@@ -51,6 +53,7 @@ func place_rect(rect: Rect2i):
 	for x in range(rect.position.x, rect.end.x):
 		for y in range(rect.position.y, rect.end.y):
 			grid_states[y + x * grid_size.x] = true
+			placed_tiles += 1
 	
 	_update_textures()
 
@@ -63,8 +66,8 @@ func _update_textures():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if gift_inside:
-		var pos = (Globals.current_gift.global_position - global_position) as Vector2i
+	if gift_inside and Globals.current_gift:
+		var pos = (Globals.current_gift.global_position - global_position) as Vector2i + Vector2i(2, 2)
 		gift_position = pos / Globals.TILE_SIZE
 		gift_global_position = (global_position as Vector2i + gift_position * Globals.TILE_SIZE) as Vector2
 
