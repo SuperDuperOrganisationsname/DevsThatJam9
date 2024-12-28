8c77d7188e0e5a4dfcea9f793ff26c158f5cd6d1
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-12-28 15:47:26 +0100

    added main menu

---
+++ scripts/buttons/menu_button.gd
@@ -0,0 +1,27 @@
+extends Button
+
+enum PossibleScenes {
+	main_game,
+	main_menu,
+	settings,
+	credits,
+}
+
+@export var scene_to_transition: PossibleScenes
+@onready var scene_path = ""
+
+func _ready() -> void:
+	match scene_to_transition:
+		0:
+			scene_path = Globals.MAIN_GAME_SCENE
+		1:
+			scene_path = Globals.MAIN_MENU_SCENE
+		2:
+			scene_path = Globals.SETTINGS_SCENE
+		3:
+			scene_path = Globals.CREDITS_SCENE
+		_:
+			scene_path = Globals.MAIN_GAME_SCENE
+
+func _pressed() -> void:
+	get_tree().change_scene_to_file(scene_path)
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,8 +1,14 @@
 extends Node
 
+# Scene Paths
+const MAIN_MENU_SCENE = "res://scenes/menus/main_menu.tscn"
+const CREDITS_SCENE = "res://scenes/menus/credits_menu.tscn"
+const SETTINGS_SCENE = ""
+const MAIN_GAME_SCENE = ""
+
 const TILE_SIZE: Vector2i = Vector2i(12, 12)
 
 var is_dragging: bool = false
 
 var current_gift: Node2D
-var current_grid: Node2D 
+var current_grid: Node2D
